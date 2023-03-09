7f703541ee78dfe651a9a46f86f68243fd5cf985
MuthuStokes <pm1085711@gmail.com>
2023-03-09 18:02:42 +0530

    22

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route }
     from 'react-router-dom';
 import Home from './pages';
-import About from './pages/about';
 import Blogs from './pages/blogs';
 import SignUp from './pages/signup';
 import Contact from './pages/contact';
+import Careers from './pages/careers';
+import Branding from './pages/branding';
+import BrandAdvertisements from './pages/brandadvertisements';
+import CompanyProfile from './pages/companyprofile';
+import DM from './pages/digitalmarketing';
+import Ecomm from './pages/ecommercewebsitedevelopement';
+import Founder from './pages/founder';
+import Portfolio from './pages/portfolio';
+import SEO from './pages/seo';
+import Web from './pages/webdesignchennai';
+import WebPortfolio from './pages/webportfolio';
+
 
 function App() {
     return (
@@ -15,9 +26,19 @@ function App() {
             <Navbar />
             <Routes>
                 <Route exact path='/' exact element={<Home />} />
-                <Route path='/about' element={<About />} />
-                <Route path='/contact' element={<Contact />} />
+                <Route path='/about' element={<CompanyProfile />} />
+                <Route path='/contact-us' element={<Contact />} />
                 <Route path='/blogs' element={<Blogs />} />
+                <Route path='/branding' element={<Branding />} />
+                <Route path='/brand-identity-advertisements' element={<BrandAdvertisements />} />
+                <Route path='/digital-marketing' element={<DM />} />
+                <Route path='/ecommerce-website-developement' element={<Ecomm />} />
+                <Route path='/founder-dream-effects' element={<Founder />} />
+                <Route path='/portfolio-brochures-product-packages' element={<Portfolio />} />
+                <Route path='/seo-search-engine-optimization' element={<SEO />} />
+                <Route path='/web-design-chennai' element={<Web />} />
+                <Route path='/web-design-portfolio' element={<WebPortfolio />} />
+                <Route path='/careers' element={<Careers />} />
                 <Route path='/sign-up' element={<SignUp />} />
             </Routes>
         </Router>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,12 @@
-
+import logo from './dream-effects-logo.svg';
+import brandicon from './img/icons/branding-designs.png';
+import wdicon from './img/icons/webdesign.png';
+import ewicon from './img/icons/ecommerce-websites.png';
+import dmicon from './img/icons/digital-marketing.png';
+import seoicon from './img/icons/seo.png';
+import pdicon from './img/icons/packaging-designs.png';
+import webicon from './img/icons/webdesign.png';
+import adicon from './img/icons/advertisements.png';
 import React from "react";
 import { Nav, NavLink, NavMenu }
     from "./NavbarElements";
@@ -6,22 +14,138 @@ import { Nav, NavLink, NavMenu }
 const Navbar = () => {
     return (
         <>
-            <Nav>
-                <NavMenu>
-                    <NavLink to="/about" activeStyle>
-                        About
-                    </NavLink>
-                    <NavLink to="/contact" activeStyle>
-                        Contact Us
-                    </NavLink>
-                    <NavLink to="/blogs" activeStyle>
-                        Blogs
-                    </NavLink>
-                    <NavLink to="/sign-up" activeStyle>
-                        Sign Up
-                    </NavLink>
-                </NavMenu>
-            </Nav>
+            <nav class='navbar navbar-expand-md  navbar-light fixed-top' id='banner'>
+                <a class='navbar-brand' href='index'><img src={logo} alt='dreameffect logo' class='img-fluid'></img></a>
+
+
+
+
+                
+                <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#collapsibleNavbar'>
+                    <i class='fa fa-bars fa-1x' aria-hidden='true'></i>
+                </button>
+                <div class='collapse navbar-collapse' id='collapsibleNavbar'>
+                    <ul class='navbar-nav ml-auto py-4 py-md-0'>
+                        <li class='nav-item'>
+                            <a class='nav-link' href='index'>Home</a>
+                        </li>
+                        <li class='nav-item dropdown'>
+                            <a class='nav-link dropdown-toggle' href='#' id='navbardrop' data-toggle='dropdown'>
+                                Services
+                            </a>
+                            <div class='dropdown-menu'>
+                                <a class='dropdown-item' href='branding'>
+                                    <div class='navcard'>
+                                        <img src={brandicon} alt='Branding Designs' class='img-fluid'></img>
+                                            <h6>Branding Designs</h6>
+                                            <span class='desc'>Logo, Brochure, Advertisements and more</span>
+                                    </div>
+                                </a>
+                                <a class='dropdown-item' href='web-design-chennai'>
+                                    <div class='navcard'>
+                                        <img src={wdicon} alt='Webdesigning Development' class='img-fluid'></img>
+                                            <h6>Web Designing & Development</h6>
+                                            <span class='desc'>Static, CMS and Custom Development</span>
+                                    </div>
+                                </a>
+
+                                <a class='dropdown-item' href='ecommerce-website-developement'>
+                                    <div class='navcard'>
+                                        <img src={ewicon} alt='Ecommerce Websites' class='img-fluid'></img>
+                                            <h6>E-Commerce</h6>
+                                            <span class='desc'>Online stores and Marketplace</span>
+                                    </div>
+                                </a>
+
+                                <a class='dropdown-item' href='digital-marketing'>
+                                    <div class='navcard'>
+                                        <img src={dmicon} alt='Digital Marketing' class='img-fluid'></img>
+                                            <h6>Digital Marketing</h6>
+                                            <span class='desc'>SEM, SMM and PPC</span>
+                                    </div>
+                                </a>
+                                <a class='dropdown-item' href='seo-search-engine-optimization'>
+                                    <div class='navcard'>
+                                        <img src={seoicon} alt='Search Engine Optimization' class='img-fluid'></img>
+                                            <h6>Search Engine Optimization</h6>
+                                            <span class='desc'>Professional Organic SEO</span>
+                                    </div>
+                                </a>
+
+                            </div>
+                        </li>
+
+                        <li class='nav-item dropdown'>
+                            <a class='nav-link dropdown-toggle' href='#' id='navbardrop' data-toggle='dropdown'>
+                                About Us
+                            </a>
+                            <div class='dropdown-menu'>
+                                <a class='dropdown-item' href='about'>Company Profile</a>
+                                <a class='dropdown-item' href='founder-dream-effects'>Founder Profile</a>
+
+                            </div>
+                        </li>
+
+
+
+                        <li class='nav-item dropdown'>
+                            <a class='nav-link dropdown-toggle' href='#' id='navbardrop' data-toggle='dropdown'>
+                                Our Works
+                            </a>
+                            <div class='dropdown-menu'>
+                                <a class='dropdown-item' href='portfolio-brochures-product-packages'>
+                                    <div class='navcard'>
+                                        <img src={pdicon} alt='Brochures Package Designs' class='img-fluid'></img>
+                                            <h6>Brochures & Package Designs</h6>
+                                            <span class='desc'>Brochures, Labels and Menu Cards Designs</span>
+                                    </div>
+                                </a>
+
+                                <a class='dropdown-item' href='web-design-portfolio'>
+                                    <div class='navcard'>
+                                        <img src={webicon} alt='Webdesigning Development' class='img-fluid'></img>
+                                            <h6>Web Designing & Development</h6>
+                                            <span class='desc'>Web Design Portfolio</span>
+                                    </div>
+                                </a>
+                                <a class='dropdown-item' href='brand-identity-advertisements'>
+
+                                    <div class='navcard'>
+                                        <img src={adicon} alt='Brand Advertisements' class='img-fluid'></img>
+                                            <h6>Brand Identity & Advertisements</h6>
+                                            <span class='desc'>Brand Advertisements</span>
+                                    </div>
+                                </a>
+                            </div>
+                        </li>
+
+
+                        <li class='nav-item'>
+                            <a class='nav-link' href='https://www.dreameffectsmedia.com/blog' target='_blank'>Blog</a>
+                        </li>
+                        <li class='nav-item'>
+                            <a class='nav-link' href='/careers' to="/about">Careers</a>
+                        </li>
+
+
+
+                        <li class='nav-item'>
+                            <a class='nav-link contact' href='contact-us'>Contact Us</a>
+                        </li>
+                        <li class='nav-item moblink'>
+                            <a class='nav-link contacticon whatsapp' href='#' onClick='sendWhatsappMessage()'><i class='fa fa-whatsapp'></i></a>
+                            <a class='nav-link contacticon' href='tel:+919444146028'><i class='fa fa-phone'></i></a>
+                        </li>
+
+
+                    </ul>
+                </div>
+            </nav>
+
+
+
+
+            
         </>
     );
 };
